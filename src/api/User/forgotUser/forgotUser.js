a96ad51e17c0001733c63cccda5512d557c10f3d
unknown <gsh0910@naver.com>
2021-03-31 17:36:26 +0900

    Add: forgotUser, sendSecretMail

--- src/api/User/forgotUser/forgotUser.js
+++ src/api/User/forgotUser/forgotUser.js
@@ -1,15 +1,42 @@
+const { generateSecret, sendSecretMail } = require("../../../utils");
+const bcrypt = require("bcrypt");
+const FINDID = "FINDID";
+const FINDPWD = "FINDPWD";
 module.exports = {
-    Mutation: {
-        forgotUser: async(_,args,{request, prisma})=>{
-            const{action, email, phoneNum} = args;
-            const{user} = request;
-            if(!user){
-                if(action == FINDID){
-                    
-                }else if(action == FINDPWD){
-
-                }
-            }
+  Mutation: {
+    forgotUser: async (_, args, { request, prisma }) => {
+      const { action, phoneNum, email } = args;
+      const { user } = request;
+      if (!user) {
+        if (action == FINDID) {
+          const existUser = await prisma.user.findMany({
+            where: { phoneNum },
+          });
+          console.log(existUser);
+          if (existUser.length == 0) {
+            throw Error("번호와 닉네임이 일치하는 유저가 없습니다.");
+          } else {
+            return existUser[0];
+          }
+        } else if (action == FINDPWD) {
+          const existUser = await prisma.user.findMany({
+            where: {
+              AND: [{ phoneNum }, { email }],
+            },
+          });
+          if (existUser.length == 0) {
+            throw Error("번호와 이메일이 일치하는 유저가 없습니다.");
+          } else {
+            const tempPWD = generateSecret();
+            console.log(tempPWD);
+            await sendSecretMail(email, tempPWD);
+            const hash = await bcrypt.hash(tempPWD, 12);
+            return await prisma.user.update({ where: { email }, data: { password: hash } });
+          }
         }
-    }
-}
+      } else {
+        throw Error("logout 후 아이디 혹은 비밀번호 찾기를 진행해주세요");
+      }
+    },
+  },
+};
--- src/utils.js
+++ src/utils.js
@@ -1,50 +1,88 @@
-const { PrismaClient } = require('@prisma/client');
-const {getNow} = require('./middlewares');
+const { PrismaClient } = require("@prisma/client");
+const { getNow } = require("./middlewares");
 const jwt = require("jsonwebtoken");
 const schedule = require("node-schedule");
+const nodemailer = require("nodemailer");
+// const sgTransport = require("nodemailer-sendgrid-transport");
+const smtpTransport = require("nodemailer-smtp-transport");
+const dotenv = require("dotenv");
 const prisma = new PrismaClient();
 
+dotenv.config();
 exports.generateToken = (id) => jwt.sign({ id }, process.env.JWT_SECRET);
 
 // 매달(매월 1일 자정) 유저들의 점수 초기화
 
-exports.resetPoint = schedule.scheduleJob("00 00 00 * * * ", async() =>{
-    let today = getNow();
-    today.setHours(9,0,0,0); //오늘의 00시 00분 00초 로 바꿈
-    let tomorrow = new Date(today);
-    tomorrow.setDate(tomorrow.getDate() + 1);
-    console.log(today, tomorrow);
-    const todayAuction = await prisma.product.findMany({
-        where: {
-            AND: [
-                {startDate : {gte : today}},
-                {startDate : {lte: tomorrow}}
-            ]
+// exports.resetPoint = schedule.scheduleJob("00 00 00 * * * ", async () => {
+//   let today = getNow();
+//   today.setHours(9, 0, 0, 0); //오늘의 00시 00분 00초 로 바꿈
+//   let tomorrow = new Date(today);
+//   tomorrow.setDate(tomorrow.getDate() + 1);
+//   console.log(today, tomorrow);
+//   const todayAuction = await prisma.product.findMany({
+//     where: {
+//       AND: [{ startDate: { gte: today } }, { startDate: { lte: tomorrow } }],
+//     },
+//   });
+//   if (todayAuction.length > 0) {
+//     for (let i = 0; i < todayAuction.length; i++) console.log("today product", todayAuction[i].id, todayAuction[i].title);
+//   }
+//   schedule.scheduleJob("00 * * * * *", async (todayAuction) => {
+//     let now = getNow();
+//     now.setSeconds(0, 0);
+//     let later = new Date(now);
+//     later.setMinutes(later.getMinutes() + 1);
+//     console.log("now", now);
+//     console.log("after 1minute", later);
+//     for (let i = 0; i < todayAuction.length; i++) {
+//       console.log(todayAuction[i].startDate);
+//       if (todayAuction[i].startDate >= now && todayAuction[i].startDate <= later) {
+//         console.log("auction 만들어", todayAuction[i].title);
+//         await prisma.auction.create({
+//           data: {
+//             currentPrice: todayAuction[i].startPrice,
+//             idSeller: todayAuction[i].idSeller,
+//             idProduct: todayAuction[i].id,
+//           },
+//         });
+//       }
+//     }
+//   });
+// });
+
+exports.generateSecret = () => {
+  let randomNumber = "";
+  for (let i = 0; i < 4; i++) {
+    randomNumber += Math.floor(Math.random() * 10);
+  }
+  return randomNumber;
+};
+
+exports.sendSecretMail = async (email, tempPWD) =>{
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        port : 587,
+        host :'smtp.gmail.com',
+        secure : false,
+        auth: {
+            user: process.env.SENDGRID_USERNAME,
+            pass: process.env.SENDGRID_PASSWORD,
+        },
+    });
+    //메일에 들어갈 정보
+    var mailOptions = {
+        from: process.env.ADMIN_EMAIL,
+        to: email,
+        subject: "🔒eleket 임시 비밀번호 발급🔒",
+        html: `임시비밀번호는 <strong>${tempPWD}</strong>입니다.<br/>`,
+        };
+
+    await transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            throw Error (error);
+        } else {
+        console.log('Email sent: ' + info.response);
         }
-    })
-    if(todayAuction.length > 0){
-        for(let i=0; i<todayAuction.length; i++)
-            console.log("today product", todayAuction[i].id, todayAuction[i].title);
-    }
-    schedule.scheduleJob("00 * * * * *", async(todayAuction) =>{
-        let now = getNow();
-        now.setSeconds(0,0);
-        let later = new Date(now);
-        later.setMinutes(later.getMinutes() + 1);
-        console.log("now", now);
-        console.log("after 1minute", later);
-        for(let i=0; i<todayAuction.length; i++){
-            console.log(todayAuction[i].startDate);
-            if(todayAuction[i].startDate >= now && todayAuction[i].startDate <= later){
-                console.log("auction 만들어", todayAuction[i].title);
-                await prisma.auction.create({
-                    data:{
-                        currentPrice: todayAuction[i].startPrice,
-                        idSeller: todayAuction[i].idSeller,
-                        idProduct: todayAuction[i].id
-                    }
-                })
-            }
-        }
-    })
-})
+    });
+    
+}
