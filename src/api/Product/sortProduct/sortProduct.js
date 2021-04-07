43ad89e6a2bc6a5bb135beb018d71179d3b813bc
윤철중 <cjfwnd123zz@gmail.com>
2021-04-07 10:17:02 +0900

    Add: 상품 정렬(남은시간순)

--- src/api/Product/sortProduct/sortProduct.js
+++ src/api/Product/sortProduct/sortProduct.js
@@ -1,4 +1,5 @@
 const NEW = "NEW";
+const FAST = "FAST";
 module.exports = {
   Query: {
     sortProduct: async (_, args, { request, prisma }) => {
@@ -11,6 +12,19 @@ module.exports = {
             }),
           },
           orderBy: [{ createdAt: "desc" }],
+        });
+        return sortedProduct;
+      }
+      if (action == FAST) {
+        // productIds에서 똑같이 받아오고 이때 startDate를 asc순, isend가 0인 경우만
+        const sortedProduct = await prisma.product.findMany({
+          where: {
+            OR: productIds.map((e) => {
+              return { id: e };
+            }),
+            AND: { isEnd: false },
+          },
+          orderBy: [{ startDate: "asc" }],
         });
         return sortedProduct;
       }
